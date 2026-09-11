e585e487316445b45643c6b95ccd0f33577ef154
madgaster5678 <ammongriner@gmail.com>
2026-09-11 11:48:18 -0700

    time to check audit page

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,11 @@
 const today = new Date()
 document.getElementById("currentyear").textContent = today.getFullYear()
 document.getElementById("lastmodified").textContent = document.lastModified
+const menuButton = document.getElementById("menu-button");
+const menu = document.getElementById("menu");
+menuButton.addEventListener("click", () => {menu.classList.toggle("menu-hidden") 
+    if (menu.classList.contains("menu-hidden")) {
+    menuButton.textContent = "☰";
+} else {
+    menuButton.textContent = "✕";
+}});
